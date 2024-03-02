aca623c70886188b743a758da952f856850251ac
shrestha7723 <ns77850617@gmail.com>
2024-03-02 05:36:49 +0100

    added end point to test send mail.

--- dentist/website/urls.py
+++ dentist/website/urls.py
@@ -3,8 +3,8 @@ from . import views
 from .views import send_mail
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('home.html', views.home, name="home"),
     path('contact.html', views.contact, name="contact"),
     path('doctors.html', views.doctors, name="doctors"),
-    path('send-mail/', send_mail, name='send_email')
+    path('send-mail/', send_mail, name='send_email') # can remove this end point
 ]
